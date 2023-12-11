04b603c7ed11ade8422ccc2021e5ec4c2ca39d8f
victor-raphael17 <victorraphaellima2005@gmail.com>
2023-12-10 22:51:20 -0300

    Update 21

--- app/service/user.service.js
+++ app/service/user.service.js
@@ -2,24 +2,18 @@ export class registerService {
   
   constructor() {}
 
-  // Constante para a chave do armazenamento local
   LOCAL_STORAGE_KEY = 'records';
 
   saveLocal(access) {
-    // Obtendo usuários do armazenamento local
+
     let register = localStorage.getItem(this.LOCAL_STORAGE_KEY);
 
-    // Verificando se há relatórios existentes
     register = register ? JSON.parse(register) : [];
 
-    // Adicionando o novo usuário ao array
     register.push(access);
 
-    // Salvando os usuários atualizados no armazenamento local
     localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(register));
-    //JSON.stringfy transforma um objeto js em uma string JSON
 
-    // Retornando os usuários atualizados
     return register;
 }
 }
